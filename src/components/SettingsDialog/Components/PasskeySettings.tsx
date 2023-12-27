2a48dc4b2057ac5f11eb35f3afafe527a9dfa0de
Benjosh95 <benjamin.fuertsch@inovex.de>
2023-12-28 00:19:20 +0100

    progess on passkey-settings-page

--- src/components/SettingsDialog/Components/PasskeySettings.tsx
+++ src/components/SettingsDialog/Components/PasskeySettings.tsx
@@ -1,14 +1,18 @@
 import {useState} from "react";
-import {SettingsAccordion} from "./SettingsAccordion";
 import store, {useAppSelector} from "store";
 import {Auth} from "utils/auth";
 import {Toast} from "utils/Toast";
 import {useTranslation} from "react-i18next";
-import { Actions } from "store/action";
+import {Actions} from "store/action";
+import {ReactComponent as KeyIcon} from "assets/icon-key.svg";
+import {ReactComponent as AddIcon} from "assets/icon-add.svg";
+import {SettingsAccordion} from "./SettingsAccordion";
+import "./PasskeySettings.scss";
+import classNames from "classnames";
 
 const PasskeySettings = () => {
   const {t} = useTranslation();
-  const [openPasskeyAccordion, setOpenPasskeyAccordion] = useState(false);
+  const [openAccordions, setOpenAccordions] = useState<{[key: string]: boolean}>({});
 
   const state = useAppSelector((applicationState) => ({
     user: applicationState.auth.user,
@@ -29,37 +33,61 @@ const PasskeySettings = () => {
 
   async function handlePasskeyDeletion(id: string) {
     try {
-        const updatedCredentials = state.user!.credentials!.filter((c) => c.ID !== id) 
-        store.dispatch(Actions.editSelf({...state.user!, credentials: updatedCredentials}, true))
+      const updatedCredentials = state.user!.credentials!.filter((c) => c.ID !== id);
+      store.dispatch(Actions.editSelf({...state.user!, credentials: updatedCredentials}, true));
     } catch (error) {
       Toast.error({title: t("Passkey could not be deleted")});
     }
-  }  
-  async function handlePasskeyRenaming() {
-    try {
-      console.log("trigger renaming")
-    } catch (error) {
-      Toast.error({title: t("Passkey could not be renamed")});
-    }
   }
-  
+
+  // TODO: add field in DB befor implementin this
+  // async function handlePasskeyRenaming() {
+  //   try {
+  //     console.log("trigger renaming");
+  //   } catch (error) {
+  //     Toast.error({title: t("Passkey could not be renamed")});
+  //   }
+  // }
+
+  const handleAccordionClick = (accordionId: string) => {
+    setOpenAccordions((prevState) => ({
+      ...prevState,
+      [accordionId]: !prevState[accordionId],
+    }));
+  };
+
+  const buildPasskeyId = (str: string, maxLength: number) => {
+    if (str.length > maxLength) {
+      return `Passkey-${str.slice(0, maxLength)}`;
+    }
+    return str;
+  };
+
   return (
-    <div className="profile-settings__passkey">
-      <SettingsAccordion isOpen={openPasskeyAccordion} onClick={() => setOpenPasskeyAccordion((prevState) => !prevState)} label="Passkeys verwalten">
-        {state.user?.credentials?.map((credential, idx) => {
+    <div className={classNames("profile-settings__passkey-container", "accent-color__lean-lilac")}>
+      <div className="profile-settings__passkey-header">
+        Passkeys Verwalten
+        <KeyIcon className="profile-settings__passkey-icon" />
+      </div>
+      <div className="profile-settings__passkey">
+        {state.user?.credentials?.map((credential) => {
           return (
-            <ul key={credential.ID}>
-              {idx + 1} {credential.ID}
+            //TODO: label = Condition if PasskeyName exists, else buildPasskeyID
+            <SettingsAccordion
+              key={credential.ID}
+              isOpen={openAccordions[credential.ID]}
+              onClick={() => handleAccordionClick(credential.ID)}
+              label={buildPasskeyId(credential.ID, 8)}
+            >
               <button onClick={() => handlePasskeyDeletion(credential.ID)}>Delete</button>
-              <button onClick={() => handlePasskeyRenaming()}>Rename</button>
-            </ul>
+            </SettingsAccordion>
           );
-          //rename, delete, lastused at createdat
         })}
-        <button onClick={() => handlePasskeyRegistration()}>+ Create a Passkey</button>
-        {/* <button onClick={() => renamePasskey()}>? Rename</button> */}
-        {/* <button onClick={() => deletePasskey()}>! Delete</button> */}
-      </SettingsAccordion>
+      </div>
+      <button className="profile-settings__passkey-create-button" onClick={() => handlePasskeyRegistration()}>
+        Create a Passkey
+        <AddIcon className="profile-settings__passkey-create-button-icon" />
+      </button>
     </div>
   );
 };
